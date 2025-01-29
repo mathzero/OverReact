ce64333dae6254999ed358c0b0884b1a65c2222a
mathzero <mattdwhitaker@gmail.com>
2025-01-29 19:22:44 +0000

    Fixed table one percentages

--- R/make_table_one.R
+++ R/make_table_one.R
@@ -79,7 +79,7 @@ tableCat <- function(dat,
 
   # statstical test
   if(statistical_test){
-    pval=chisq.test(tab)
+    pval=suppressWarnings(chisq.test(tab))
   }
 
 
@@ -94,7 +94,7 @@ tableCat <- function(dat,
   # tab <- addmargins(table(pull(dat,rowvar), pull(dat,colvar))) %>% as.data.frame.matrix()
   if(rowwise_precentages){
     tab.prop <- round(100*prop.table(table(rowvect, colvect),1),1) %>% as.data.frame.matrix()
-    tab.prop[,"sum"] <- 100
+    tab.prop[,"sum"] <- ""
   }else{
     tab.prop <- round(100*prop.table(table(rowvect, colvect),2),1) %>% as.data.frame.matrix()
     tab.prop[,"sum"] <- round(100*prop.table(table(rowvect)),1)
@@ -119,14 +119,17 @@ tableCat <- function(dat,
             tab[i,j] <-  paste0(tab[i,j], " (",round(100*ci$p,1),"%)")
           }else{
             tab[i,j] <-  paste0(tab[i,j], " (",round(100*ci$p,1),"%, [",round(100*ci$lower,1),"-",round(100*ci$upper,1),"])")
-
           }
         }
       }
     }else{
       for (i in 1:(nrow(tab))){
         for(j in 1:(ncol(tab))){
-          tab[i,j] <-  paste0(tab[i,j], " (",tab.prop[i,j],"%)")
+          if(j==ncol(tab)){
+            tab[i,j] <-  as.character(tab[i,j])
+          }else{
+            tab[i,j] <-  paste0(tab[i,j], " (",tab.prop[i,j],"%)")
+          }
         }
       }
       # for(j in 1:ncol(tab)){
@@ -316,8 +319,12 @@ tableOne <- function(dat,
                      statistical_test = F,
                      includeNAsColvar=T,
                      includeNAsRowvar=T,
-                     formatPvalsForEpiPaper=T,
+                     formatPvalsForEpiPaper=F,
                      addNobsTopRow=T){
+
+  if(!statistical_test){
+    formatPvalsForEpiPaper=F
+  }
 
   if(!summary_stat %in% c("mean","median")){
     print("Please choose 'mean' or 'median' for summary_stat")
